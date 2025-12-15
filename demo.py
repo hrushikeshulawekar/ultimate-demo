4864c73ab1d2e9c8d912cfebc1f31e21dc8ac803
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2025-12-15 15:30:38 +0530

    Implement basic arithmetic functions in demo.py

---
+++ demo.py
@@ -0,0 +1,16 @@
+def add(a,b):
+  print(a+b)
+def min(a,b):
+  print(a-b)
+def mul(a*b):
+  print(a*b)
+def div(a/b):
+  print(a/b)
+
+def test(a,b):
+  add(a,b)
+  min(a,b)
+  mul(a,b)
+  div(a,b)
+
+test(4,6)
