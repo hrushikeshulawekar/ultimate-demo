0f0fac863230aafcbb07bd56edd3c2926a16afb0
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2025-12-16 10:40:00 +0530

    Add login function with success and error messages
    Implement basic login functionality with username and password checks.

---
+++ login.py
@@ -0,0 +1,19 @@
+def success():
+  print("login successful")
+
+def wrong_user():
+  print("username is wrong, cant login")
+
+def wrong_password():
+  print("password is wrong")
+
+def login(username,password):
+  if username == "admin":
+    if password == "root@1234":
+      success()
+    else:
+      wrong_password()
+  else:
+    wrong_user()
+
+login(admin,root@1234)
