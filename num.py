85ac9d90a6b4058ae4ee944f5837607d3c40a874
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2025-12-17 15:24:16 +0530

    Add functions to check even and odd numbers

---
+++ num.py
@@ -0,0 +1,15 @@
+def even():
+  print("it is even number")
+
+def odd():
+  print("It is odd number")
+
+def test(a):
+  if a%2==0:
+    even()
+  else:
+    odd()
+
+test(2)
+print("version 1.0")
+  
