02635c503d2dec0d0ffc23ca26bb138ca42f2cce
hrushikeshulawekar <hrushikesh.s.ulawekar@gmail.com>
2025-12-18 16:00:09 +0530

    Add age validation functions and check logic

---
+++ age.py
@@ -0,0 +1,15 @@
+# age validation
+
+def valid():
+  print("you are 18+, you are allowed")
+
+def not_valid():
+  print("You are not mature enough, you are not allowed")
+
+def check(age):
+  if age>=18:
+    valid()
+  else:
+    not_valid()
+
+check(18)
